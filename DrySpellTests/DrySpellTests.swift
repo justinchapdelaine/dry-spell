908bf2a87e00694d8196f418d4a1d06e7b7c117f
Justin Chapdelaine <10560997+justinchapdelaine@users.noreply.github.com>
2026-04-17 14:53:07 -0700

    Initial Commit

---
+++ DrySpellTests/DrySpellTests.swift
@@ -0,0 +1,19 @@
+//
+//  DrySpellTests.swift
+//  DrySpellTests
+//
+//  Created by Justin Chapdelaine on 2026-04-17.
+//
+
+import Testing
+@testable import DrySpell
+
+struct DrySpellTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
